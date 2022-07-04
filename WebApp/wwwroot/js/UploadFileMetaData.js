8c9954cc1f1ae19a1589bc286def159cb39dd234
Maryll Castelino <maryllcastelino@gmail.com>
2022-07-04 22:49:40 +0530

    merge mistake fix showMetadata function appeared twice in the code due to incorrect merging.

--- WebApp/wwwroot/js/UploadFileMetaData.js
+++ WebApp/wwwroot/js/UploadFileMetaData.js
@@ -2,7 +2,7 @@
 // show metadata of uploaded file
 var showMetadata = function (e, fileId) {
     var url = "/Home/FileMetadata?file=" + fileId;
-    $("li").removeClass("active");
+    $("a.active").removeClass("active");
     e.classList.add("active");
     $("#metadata").load(url);
 
